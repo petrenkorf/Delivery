5cf1bb0f990f2b9e344f0307ff01d3cd437106a7
Petris Rodrigo Fernandes <petrisrf@gmail.com>
2018-05-16 22:53:16 -0300

    cities delivery

--- lib/city.rb
+++ lib/city.rb
@@ -1,7 +1,13 @@
 class City
-  attr_reader :pub
+  attr_reader :pub, :name
   
-  def initialize(pub)
-    @pub = pub
+  def initialize(name, pub)
+    @pub  = pub
+    @name = name
+  end
+
+  def has_delivery?()
+    return false if @name == "Balneario Camboriu"
+    return true
   end
 end
--- spec/city_spec.rb
+++ spec/city_spec.rb
@@ -4,7 +4,34 @@ require 'pub'
 RSpec.describe City do
   it "city has pub" do
     pub = Pub.new
-    city = City.new pub
+    city = City.new "Balneario Camboriu", pub
     expect(city.pub).to be(pub)
+  end
+  
+  context "Balneario Camboriu's pub" do
+    pub = Pub.new
+    city = City.new "Balneario Camboriu", pub
+
+    it "does not delivery orders" do
+      expect(city.has_delivery?).to be(false)    
+    end
+  end
+
+  context "Itajai's pub" do
+    pub = Pub.new
+    city = City.new "Itajai", pub
+
+    it "delivery orders" do
+      expect(city.has_delivery?).to be(true)    
+    end
+  end
+
+  context "Itapema's pub" do
+    pub = Pub.new
+    city = City.new "Itapema", pub
+
+    it "delivery orders" do
+      expect(city.has_delivery?).to be(true)    
+    end
   end
 end
