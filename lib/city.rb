00c8f954f279d49ffb3879e10eaf9dbbf35d586a
Petris Rodrigo Fernandes <petrisrf@gmail.com>
2018-05-18 20:15:52 -0300

    Delivery price

--- lib/city.rb
+++ lib/city.rb
@@ -1,8 +1,9 @@
 class City
-  attr_reader :name
-  
-  def initialize(name)
+  attr_reader :name, :delivery_price
+
+  def initialize(name, delivery_price = 10.00)
     @name = name
+    @delivery_price = delivery_price
   end
 
   def delivery?()
@@ -13,5 +14,10 @@ class City
   def local_withdraw?()
     return false if @name == "Itajai"
     return true
+  end
+
+  def delivery_to?(address)
+    return true if @name == address.city or @name == "Itajai"
+    return false
   end
 end
--- spec/city_spec.rb
+++ spec/city_spec.rb
@@ -1,11 +1,25 @@
 require 'city'
 
 RSpec.describe City do
-  it "city has pub" do
+  it "city has name" do
     city = City.new "Balneario Camboriu"
     expect(city.name).to eq("Balneario Camboriu")
   end
   
+  it "costs R$ 10,00 to delivery" do
+    city = City.new "Itapema"
+    expect(city.delivery_price).to eq(10.00)
+  end
+
+  it "delivery in same city only" do
+      city         = City.new "Balneario Camboriu"
+      same_city    = Address.new "Balneario Camboriu"
+      another_city = Address.new "Itajai"
+
+      expect(city.delivery_to?(same_city)).to be(true)
+      expect(city.delivery_to?(another_city)).to be(false)
+  end
+
   context "Balneario Camboriu's pub" do
     city = City.new "Balneario Camboriu"
 
@@ -28,6 +42,11 @@ RSpec.describe City do
     it "doesn't have local withdraw" do
       expect(city.local_withdraw?).to be(false)
     end
+    
+    it "delivery to other cities" do
+      different_city = Address.new "Itapema"
+      expect(city.delivery_to? different_city).to be(true)
+    end
   end
 
   context "Itapema's pub" do
@@ -40,5 +59,6 @@ RSpec.describe City do
     it "have local withdraw" do
       expect(city.local_withdraw?).to be(true)
     end
+
   end
 end
